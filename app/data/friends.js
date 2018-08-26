c85f17bd7a23b4c161dbe3bfa0e8d92f41b19a9d
william zhu <william_zhu@hotmail.ca>
2018-08-26 02:49:17 -0400

    changed pictures and names

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,23 @@
 friendsArry = [
 {
-    "name": 'bob',
-    "photo": 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+    "name": 'Saitima',
+    "photo": 'https://www.geek.com/wp-content/uploads/2017/06/saitamamain.png',
     "scores": [
         5,
-        1,
-        2,
-        3,
-        1,
-        1,
-        2,
         5,
-        4,
-        1
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
     ]
 },
 {
-    "name": 'sam',
-    "photo": 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+    "name": 'Nyanko-sensei',
+    "photo": 'https://i.stack.imgur.com/NTTTK.jpg',
     "scores": [
         5,
         1,
@@ -32,8 +32,8 @@ friendsArry = [
       ]
   },
   {
-    "name": 'samhello',
-    "photo": 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+    "name": 'Rem',
+    "photo": 'https://thehypedgeek.com/wp-content/uploads/2017/04/rem-re-zero.jpg',
     "scores": [
         5,
         1,
@@ -48,88 +48,88 @@ friendsArry = [
       ]
   },
   {
-    "name": 'guy',
-    "photo": 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+    "name": 'Drago',
+    "photo": 'http://themeditationteacher.net/wp-content/uploads/2017/06/8af03534754a8f98ee8c50fdb2826da1.jpg',
     "scores": [
         5,
+        4,
+        5,
+        4,
+        5,
+        4,
         5,
         5,
+        4,
+        4
+      ]
+  },
+  {
+    "name": 'CHOU-CHOU',
+    "photo": 'http://www.opwiki.de/images/Shushu.PNG',
+    "scores": [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+      ]
+  },
+  {
+    "name": 'Happy',
+    "photo": 'https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/5b3a7585575d1f721475b3ba/1530557834084/happy_fairy_tail.jpg',
+    "scores": [
+        3,
         5,
+        2,
+        4,
         5,
+        1,
+        4,
         5,
+        1,
+        1
+      ]
+  },
+  {
+    "name": 'Samurai Jack',
+    "photo": 'http://media.comicbook.com/2017/10/36190303uan-samuraijack-1449189177030-1280w-skmh-1038393.jpg',
+    "scores": [
         5,
-        5,
-        5,
+        4,
+        4,
+        4,
+        2,
+        1,
+        2,
+        4,
+        4,
+        1
+      ]
+  },
+  {
+    "name": 'chocobo',
+    "photo": 'https://vignette.wikia.nocookie.net/finalfantasy/images/a/a6/Choco.jpg/revision/latest?cb=20111201180545',
+    "scores": [
+        4,
+        2,
+        4,
+        4,
+        3,
+        1,
+        2,
+        1,
+        2,
         5
       ]
   },
   {
-    "name": 'guytim',
-    "photo": 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
-    "scores": [
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1
-      ]
-  },
-  {
-    "name": 'yo',
-    "photo": 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
-    "scores": [
-        3,
-        5,
-        2,
-        4,
-        5,
-        1,
-        4,
-        5,
-        1,
-        1
-      ]
-  },
-  {
-    "name": 'hello',
-    "photo": 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
-    "scores": [
-        5,
-        4,
-        4,
-        4,
-        2,
-        1,
-        2,
-        4,
-        4,
-        1
-      ]
-  },
-  {
-    "name": 'sup',
-    "photo": 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
-    "scores": [
-        4,
-        2,
-        4,
-        4,
-        3,
-        1,
-        2,
-        1,
-        2,
-        5
-      ]
-  },
-  {
-    "name": 'man',
-    "photo": 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+    "name": 'Pikachu',
+    "photo": 'https://qph.fs.quoracdn.net/main-qimg-8da972b6e2138902fdebf133cd73a4f6',
     "scores": [
         5,
         1,
@@ -144,8 +144,8 @@ friendsArry = [
       ]
   },
   {
-    "name": 'Scooter',
-    "photo": 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+    "name": 'inuyasha',
+    "photo": 'https://vignette.wikia.nocookie.net/inuyasha/images/b/b5/Inuyasha.png/revision/latest?cb=20151128185518',
     "scores": [
         5,
         1,
